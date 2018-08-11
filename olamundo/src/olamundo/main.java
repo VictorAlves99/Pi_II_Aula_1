f018f3c2439c168904a65df39bc9c29cf03af6fc
XSCMCASDK <t-xs-159754@senacsp.edu.br>
2018-08-10 22:09:56 -0300

    first commit

---
+++ olamundo/src/olamundo/main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olamundo;
+
+import javax.swing.JFrame;
+
+public class main {
+    public static void main(String[] args) {
+        TelaInicial tela1 = new TelaInicial(); 
+        tela1.setVisible(true);
+    }
+    
+}
